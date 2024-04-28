d75fee1df71cf71f8f7672f9dccef53154a2086c
Matthew Wentworth <wentworthm@gmail.com>
2024-04-28 09:52:30 -0400

    initial commit with a working fibonacci fast app page and a git ignore file

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+import math
+from typing import Union
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/isFibonacci/{fib_num}")
+def read_item(fib_num: int, q: Union[str, None] = None):
+    return fib_num if isFibonacci(fib_num) else "not a Fibonacci number."
+
+def isPerfectSquare(x):
+    s = int(math.sqrt(x))
+    return s * s == x
+
+def isFibonacci(n):
+    return isPerfectSquare(5 * n * n + 4) or isPerfectSquare(5 * n * n - 4)
